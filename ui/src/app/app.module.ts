7947282480ecae2212e0573917db2d37f0f12953
bharathbattula1 <bharathbattula1@gmail.com>
2020-05-31 20:52:21 +0530

    install ngrx dependencies and changed module file

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -25,6 +25,10 @@ import {ConfirmationComponent} from "./dialog/confirmation/confirmation.componen
 import {TaskDetailComponent} from './board/task-detail/task-detail.component';
 import {ProjectSettingComponent} from './project-setting/project-setting.component';
 import {MatTableModule} from "@angular/material/table";
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 
 @NgModule({
@@ -54,6 +58,9 @@ import {MatTableModule} from "@angular/material/table";
         MatAutocompleteModule,
         FormsModule,
         MatTableModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        EffectsModule.forRoot([]),
 
     ],
   providers: [
