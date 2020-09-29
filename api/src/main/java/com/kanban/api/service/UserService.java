42a7b513be5ff8cdda36ae09b63f83d64da5ef80
bharathbattula1 <bharathbattula1@gmail.com>
2020-09-29 15:59:01 +0530

    moved signup logic to userservice class

--- api/src/main/java/com/kanban/api/service/UserService.java
+++ api/src/main/java/com/kanban/api/service/UserService.java
@@ -1,10 +1,14 @@
 package com.kanban.api.service;
 
+import com.kanban.api.exception.BadRequestException;
 import com.kanban.api.model.User;
 import com.kanban.api.repository.UserRepository;
+import com.kanban.api.request.SignUpDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +21,41 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
-	public List<User> loadMatchingUsers(final String email) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> loadMatchingUsers(final String email) {
 		return this.userRepository.findAllByEmailContains(email);
 	}
+
+    public User saveUser(final SignUpDto signUpDto) {
+
+        try {
+
+            if (this.userRepository.existsByUsername(signUpDto.getUsername())) {
+                throw new BadRequestException("Username already exist");
+            }
+
+            if (this.userRepository.existsByEmail(signUpDto.getEmail())) {
+                throw new BadRequestException("Email already exist");
+            }
+
+            final User user = new User(signUpDto.getFirstName(), signUpDto.getLastName(), signUpDto.getUsername(),
+                    signUpDto.getEmail(), signUpDto.getPassword());
+
+            user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+
+            return this.userRepository.save(user);
+
+        } catch (final Exception e) {
+
+            if (e instanceof DataIntegrityViolationException) {
+                LOGGER.error("Duplicate username or email : {}, {}", signUpDto.getUsername(), signUpDto.getEmail(), e);
+                throw new BadRequestException("Username or Email already exist");
+            }
+
+            LOGGER.error("error saving user", e);
+            throw e;
+        }
+    }
 }
