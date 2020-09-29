25be4ecb0287b08f4c2742775e355e0a734e5d29
bharathbattula1 <bharathbattula1@gmail.com>
2020-09-29 15:56:23 +0530

    Revert "basic architecture ngrx state for board management"
    This reverts commit fa0165eee88427fb5b04e9ddc0859daaa6cc1448.

--- ui/src/app/board/state/board.state.ts
+++
@@ -1,9 +0,0 @@
-import {Project} from '../../shared/model/Project';
-import {List} from '../../shared/model/List';
-
-export interface ProjectDetails {
-  project: Project
-  list: List[];
-}
-
-export const initialState: ProjectDetails = {} as ProjectDetails;
--- ui/src/app/board/state/index.ts
+++
@@ -1,3 +0,0 @@
-export {reducer} from './board.reducer';
-export * from './board.actions';
-export { ProjectDetails, initialState } from './board.state';
